5755c59e4732473733d48e5b5c339419b4577a56
Hoooki <hooki.kibo@gmail.com>
2020-11-11 11:17:34 +0100

    Fixtures

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class CategoryFixtures extends Fixture
+{
+
+    private $randomCategory = [
+
+        'Sport',
+        'Musical',
+        'Games'
+
+    ];
+
+    public function load(ObjectManager $manager) : void
+    {
+
+        $faker = Factory::create();
+
+        for($i = 0; $i < 3; $i++) {
+
+            $category = new Category();
+
+            $category->setName($faker->randomElement($this->randomCategory));
+            $category->setDescription($faker->sentence(36));
+
+        }
+
+        $manager->flush();
+    }
+}
